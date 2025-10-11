fb5882547b25b8e6ae17cf4f45bea5b75de4579c
Mayank67-tech <mayank92.6agarwal@gmail.com>
2025-10-11 20:39:58 +0530

    corrected error in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <Toaster
           toastOptions={{
             style: { fontSize: "13px" },
-            
+            success: { icon: '✅' },
             error: { icon: '❌' },
           }}
         />
